cadd412a8cb5bebc25bef290baa8529b0713cc11
Oleg <bzbms@ya.ru>
2024-08-20 12:45:51 +0300

    ТЗ-6 - исправления по первой проверке.

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -3,13 +3,14 @@ package managers;
 public class Managers {
     // Приватный конструктор, чтобы не позволять создавать экземпляры этого класса.
     // Так класс становится более утилитарным по словам Наставника, но мы кажется ещё не проходили этого. :В
-    private Managers(){}
+    private Managers() {
+    }
 
-    public static TaskManager getDefaultManager(){
+    public static TaskManager getDefaultManager() {
         return new InMemoryTaskManager();
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
